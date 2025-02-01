d2bc7a47a69fe7bfd3b4caa1fb685279186debad
OrestHutovych <horuzart001@gmail.com>
2025-02-01 13:15:06 +0200

    commit 0.98 Added validation for identifier when is creating

--- src/main/java/org/example/kihelp_back/task/usecase/impl/TaskCreateUseCaseFacade.java
+++ src/main/java/org/example/kihelp_back/task/usecase/impl/TaskCreateUseCaseFacade.java
@@ -8,6 +8,7 @@ import org.example.kihelp_back.task.usecase.TaskCreateUseCase;
 import org.springframework.stereotype.Component;
 
 import static org.example.kihelp_back.task.util.TaskErrorMessage.IDENTIFIER_BLANK_NOT_VALID;
+import static org.example.kihelp_back.task.util.TaskErrorMessage.IDENTIFIER_NOT_VALID;
 
 @Component
 public class TaskCreateUseCaseFacade implements TaskCreateUseCase {
@@ -22,16 +23,23 @@ public class TaskCreateUseCaseFacade implements TaskCreateUseCase {
 
     @Override
     public void createTask(TaskCreateDto taskRequest) {
-        if (taskRequest.autoGenerate()) {
-            String identifier = taskRequest.identifier();
+        String identifier = taskRequest.identifier();
 
+        if (taskRequest.autoGenerate()) {
             if (identifier == null || identifier.isBlank()) {
                 throw new IllegalArgumentException(IDENTIFIER_BLANK_NOT_VALID);
+            }
+            if (!(identifier.equals("task") || identifier.equals("main"))) {
+                throw new IllegalArgumentException(String.format(IDENTIFIER_NOT_VALID, "task або main"));
+            }
+        } else {
+            if (!"google_drive".equals(identifier)) {
+                throw new IllegalArgumentException(String.format(IDENTIFIER_NOT_VALID, "google_drive"));
             }
         }
 
         Task task = taskMapper.toEntity(taskRequest);
-
         taskService.create(task);
     }
+
 }
--- src/main/java/org/example/kihelp_back/task/util/TaskErrorMessage.java
+++ src/main/java/org/example/kihelp_back/task/util/TaskErrorMessage.java
@@ -8,6 +8,7 @@ public class TaskErrorMessage {
     public static final String TYPE_NOT_VALID = "Тип завдання '%s' не знайдено.";
     public static final String TITLE_BLANK_NOT_VALID = "Заголовок завдання не може бути порожнім.";
     public static final String IDENTIFIER_BLANK_NOT_VALID = "Ідентифікатор завдання не може бути порожнім.";
+    public static final String IDENTIFIER_NOT_VALID = "Ідентифікатор має містити %s";
     public static final String PRICE_MIN_NOT_VALID = "Ціна повинна бути більшою за 1 UAH.";
     public static final String USER_BANNED_BY_RESELLER_ACTIVITY = "Користувача із Telegram ID %s було забанено за підозрілу активність. Bиявлено повторні спроби масового придбання завдань.";
     public static final String ARGS_NOT_NULL = "Завдання не може мати null аргументів";
